8dad2e47827b1a01ad46a1739a4f1d8b02d3d7a7
Amadu <amaduhamzajaarah300@gmail.com>
2022-12-13 15:53:31 -0800

    my program 1

---
+++ area_of_circle.c
@@ -0,0 +1,29 @@
+#include <stdio.h> 
+/*function to calculate the area of a circle
+ *using function withougt arguement or return value
+ */
+
+void area(); //declaring the fubction prototype
+
+void main(){
+
+	area(); // Calling the function in main function
+
+	}
+
+//defining my funcrion
+void area() {
+       
+	 float area_circle ;
+	 float radius ;
+      
+	 printf("\n Enter the radius" :) ;
+         scanf("%f", radius)
+
+		 area_circle = 3.14 * radius * radius ;
+	 printf("The area of the circle = %f", area_circle ;
+
+
+
+
+}
